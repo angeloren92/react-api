635396b08f6509d6d077508eee822564864d7649
angeloren92 <angeloren92@gmail.com>
2026-03-17 12:35:28 +0100

    add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import AppHeader from './components/AppHeader'
 import AppMain from './components/AppMain'
 import AppFooter from './components/AppFooter'
-import { useEffect, useState } from 'react'
 
 function App() {
 
   const apiUrl = 'https://lanciweb.github.io/demo/api/actresses/'
-  const [lists, setLists] = useState([])
-
-  useEffect(() => {
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        setLists(data)
-        console.log(data)
-      })
-  }, [])
-
 
   return (
     <>
       <AppHeader />
       <AppMain
-        lists={lists}
+        apiUrl={apiUrl}
       />
       <AppFooter />
     </>
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,8 +1,50 @@
-export default function AppMain({lists}) {
+import { useEffect, useState } from 'react'
 
+export default function AppMain({ apiUrl }) {
+
+    const [lists, setLists] = useState([])
+    console.log(lists)
+
+    useEffect(() => {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                setLists(data)
+            })
+    }, [])
 
     return (
-        <>
-        </>
+        <main>
+            <div className="container">
+                <div className="row">
+                    {
+                        lists.map(element => {
+                            return (
+                                <div className="col-4 gy-3" key={element.id}>
+                                    <div className="card bg-dark h-100">
+                                        <h1 className="card-title text-white text-center">
+                                            {element.name}
+                                        </h1>
+                                        <div className="card-body d-flex align-items-center justify-content-center text-white">
+                                            <img src={element.image} alt={element.name} className='img-fluid w-75' />
+                                            <small>{element.birth_year}</small>
+                                            <p>
+
+                                            </p>
+                                            <p>
+
+                                            </p>
+                                            <p>
+
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </main>
     )
 }
