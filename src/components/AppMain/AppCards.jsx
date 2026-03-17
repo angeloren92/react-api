aa653e439bb19277243a6fd1c1ea7d2572275f64
angeloren92 <angeloren92@gmail.com>
2026-03-17 14:48:29 +0100

    adj CSS

--- src/components/AppMain/AppCards.jsx
+++ src/components/AppMain/AppCards.jsx
@@ -2,22 +2,22 @@ export default function AppCards({ element }) {
 
     return (
         < div className="col-4 gy-3" key={element.id} >
-            <div className="card bg-dark h-100">
-                <h2 className="card-title text-white text-center mb-0 mt-3">
+            <div className="card bg-dark rounded-4 h-100 d-flex flex-column align-items-center text-white text-center">
+                <h2 className="card-title mb-1 mt-3">
                     {element.name}
                 </h2>
-                <div className="card-body d-flex flex-column align-items-center text-white text-center pt-0">
-                    <img src={element.image} alt={element.name} className='img-fluid w-75' />
-                    <span className="d-flex gap-3 mb-3">
-                        <small>{element.birth_year}</small>
-                        <small>{element.nationality}</small>
-                    </span>
+                <img src={element.image} alt={element.name} className='img-fluid w-75' />
+                <span className="d-flex fst-italic gap-3 mb-3">
+                    <small>{element.birth_year}</small>
+                    <small>{element.nationality}</small>
+                </span>
+                <div className="card-body d-flex flex-column align-items-center justify-content-between text-white text-center pt-0">
                     <p>{element.biography}</p>
-                    <p className="text-danger d-flex flex-column">
+                    <p className="text-danger d-flex flex-column fst-italic">
                         <span className="fw-bold">Know For</span>
                         <span>{element.known_for.join(', ')}</span>
                     </p>
-                    <p className="text-warning d-flex flex-column">
+                    <p className="text-warning d-flex flex-column fst-italic">
                         <span className="fw-bold">Awards</span>
                         <span>{element.awards.join(', ')}</span>
                     </p>
