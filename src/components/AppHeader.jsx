a6c3fa8967a655f3b9a763e3650e31ed9320b5fe
angeloren92 <angeloren92@gmail.com>
2026-03-17 11:53:13 +0100

    add header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,7 +2,14 @@ export default function AppHeader() {
 
 
     return (
-        <>
-        </>
+        <header>
+            <div className="container-fluid p-0">
+                <p className="bg-dark text-white text-center p-3">Wonderful Cast</p>
+                <div className="d-flex justify-content-center align-items-center flex-column my-4">
+                <h1>Actors</h1>
+                <small>List of actors fetched from an API</small>
+                </div>
+            </div>
+        </header>
     )
 }
