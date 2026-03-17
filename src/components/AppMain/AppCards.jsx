7568c084b84c6fd18754d9fd616a98ab4f0337ba
angeloren92 <angeloren92@gmail.com>
2026-03-17 13:00:45 +0100

    adj card

--- src/components/AppMain/AppCards.jsx
+++ src/components/AppMain/AppCards.jsx
@@ -5,21 +5,13 @@ export default function AppCards({ element }) {
     return (
         < div className="col-4 gy-3" key={element.id} >
             <div className="card bg-dark h-100">
-                <h1 className="card-title text-white text-center">
+                <h2 className="card-title text-white text-center mb-0 mt-3">
                     {element.name}
-                </h1>
-                <div className="card-body d-flex flex-column align-items-center justify-content-center text-white">
+                </h2>
+                <div className="card-body d-flex flex-column align-items-center text-white pt-0">
                     <img src={element.image} alt={element.name} className='img-fluid w-75' />
                     <small>{element.birth_year} {element.nationality}</small>
-                    <p>
-
-                    </p>
-                    <p>
-
-                    </p>
-                    <p>
-
-                    </p>
+                    <p>{element.biography}</p>
                 </div>
             </div>
         </div>
